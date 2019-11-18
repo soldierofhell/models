16f3fd19b61579610e5deb4c92c3546dbb6c872c
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-11-18 13:28:35 +0100

    Update anchor.py

--- official/vision/detection/dataloader/anchor.py
+++ official/vision/detection/dataloader/anchor.py
@@ -290,3 +290,73 @@ class RpnAnchorLabeler(AnchorLabeler):
     box_targets_dict = self._anchor.unpack_labels(box_targets)
 
     return score_targets_dict, box_targets_dict
+  
+class YOLOV3Anchor(object):
+  """Anchor class for anchor-based object detectors."""
+
+  def __init__(self,
+               anchors,
+               anchor_masks,
+               image_size):
+    """Constructs multiscale anchors.
+
+    Args:
+
+      image_size: a list of integer numbers or Tensors representing
+        [height, width] of the input image size.The image_size should be divided
+        by the largest feature stride 2^max_level.
+    """
+    self.anchors = anchors
+    self.anchor_masks = anchor_masks
+    self.image_size = image_size
+    self.boxes = self._generate_boxes()
+
+  def _generate_boxes(self):
+    """Generates multiscale anchor boxes.
+
+    Returns:
+      a Tensor of shape [N, 4], represneting anchor boxes of all levels
+      concatenated together.
+    """
+    boxes_all = []
+    for level in range(len(anchor_masks)):
+      boxes_l = []
+      stride = 2 ** level
+      half_anchor_size_x = base_anchor_size * aspect_x / 2.0
+      half_anchor_size_y = base_anchor_size * aspect_y / 2.0
+      x = tf.range(stride / 2, self.image_size[1], stride)
+      y = tf.range(stride / 2, self.image_size[0], stride)
+      xv, yv = tf.meshgrid(x, y)
+      xv = tf.cast(tf.reshape(xv, [-1]), dtype=tf.float32)
+      yv = tf.cast(tf.reshape(yv, [-1]), dtype=tf.float32)
+      # Tensor shape Nx4.
+      boxes = tf.stack([yv - half_anchor_size_y, xv - half_anchor_size_x,
+                        yv + half_anchor_size_y, xv + half_anchor_size_x],
+                       axis=1)
+      boxes_l.append(boxes)
+      # Concat anchors on the same level to tensor shape NxAx4.
+      boxes_l = tf.stack(boxes_l, axis=1)
+      boxes_l = tf.reshape(boxes_l, [-1, 4])
+      boxes_all.append(boxes_l)
+    return tf.concat(boxes_all, axis=0)
+
+  def unpack_labels(self, labels):
+    """Unpacks an array of labels into multiscales labels."""
+    unpacked_labels = collections.OrderedDict()
+    count = 0
+    for level in range(self.min_level, self.max_level + 1):
+      feat_size_y = tf.cast(self.image_size[0] / 2 ** level, tf.int32)
+      feat_size_x = tf.cast(self.image_size[1] / 2 ** level, tf.int32)
+      steps = feat_size_y * feat_size_x * self.anchors_per_location
+      unpacked_labels[level] = tf.reshape(
+          labels[count:count + steps], [feat_size_y, feat_size_x, -1])
+      count += steps
+    return unpacked_labels
+
+  @property
+  def anchors_per_location(self):
+    return self.num_scales * len(self.aspect_ratios)
+
+  @property
+  def multilevel_boxes(self):
+    return self.unpack_labels(self.boxes)
